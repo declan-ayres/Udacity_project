9af26206522c27fcea6f449b205cab73d4b51473
Ubuntu <ubuntu@ip-172-31-18-103.us-west-2.compute.internal>
2016-10-19 00:46:02 +0000

    updated the extract_glyphs file

--- extract_glyphs.py
+++ extract_glyphs.py
@@ -8,7 +8,7 @@ import numpy as np
 filehandle = open('allfonts-paths-from-server', 'r')
 files_list = filehandle.readlines()
 path = 'glyphs_output/'
-excluding_list = ['/usr/share/ghostscript/9.10/Resource/CIDFSubst/DroidSansFallback.ttf', '/usr/share/texlive/texmf-dist/fonts/truetype/public/dejavu/DejaVuSans-BoldOblique.ttf','/usr/share/texlive/texmf-dist/fonts/truetype/public/gentium-tug/GentiumPlus-I.ttf', '/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf','/usr/share/doc/texlive-doc/fonts/cm-unicode/Fontmap.CMU.otf','/usr/share/texlive/texmf-dist/fonts/opentype/public/inconsolata/Inconsolata.otf']
+excluding_list = ['/usr/share/ghostscript/9.10/Resource/CIDFSubst/DroidSansFallback.ttf', '/usr/share/texlive/texmf-dist/fonts/truetype/public/dejavu/DejaVuSans-BoldOblique.ttf','/usr/share/texlive/texmf-dist/fonts/truetype/public/gentium-tug/GentiumPlus-I.ttf', '/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf','/usr/share/doc/texlive-doc/fonts/cm-unicode/Fontmap.CMU.otf','/usr/share/texlive/texmf-dist/fonts/opentype/public/inconsolata/Inconsolata.otf','/usr/share/fonts/truetype/inconsolata/Inconsolata.otf']
 for j,i in enumerate(files_list):
         i = i.rstrip('\n')
 	if i in excluding_list or "DejaVu" in i:
@@ -28,22 +28,25 @@ for j,i in enumerate(files_list):
 	unique_unicodes = {}
 	cnt = 0
 	for g in glyphsare:
-		if g.unicode not in unique_unicodes:
-			unique_unicodes[g.unicode] = 0
-			continue
-		glyph_decimal_code = unique_unicodes[g.unicode]
+	#	if g.unicode not in unique_unicodes:
+	#		unique_unicodes[g.unicode] = 0
+	#		continue
+		#glyph_decimal_code = unique_unicodes[g.unicode]
+                print("unicode = ",g.unicode)
+                if g.unicode <0:
+                    continue
 		intrinsic_glyph_decimal_code = g.unicode
                 list_of_files=[]
-		g.export(path + os.path.basename(i)+'-orig-'+ '.bmp')
-		g.transform(psMat.skew(.0523599)).export(path + os.path.basename(i)+'-skew-'+ '.bmp')
-		list_of_files.append(path+os.path.basename(i) + '-orig-' + '.bmp')
-		list_of_files.append(path+os.path.basename(i) + '-skew-' + '.bmp')
+		g.export(path + os.path.basename(i)+'-orig-'+ str(g.unicode) + '.bmp')
+		g.transform(psMat.skew(.0523599)).export(path + os.path.basename(i)+'-skew-'+ str(g.unicode)+ '.bmp')
+		list_of_files.append(path+os.path.basename(i) + '-orig-' + str(g.unicode)+ '.bmp')
+		list_of_files.append(path+os.path.basename(i) + '-skew-' + str(g.unicode)+ '.bmp')
 
-		g.transform(psMat.rotate(.0523599)).export(path + os.path.basename(i)+'-rotate-'+ '.bmp')
-		list_of_files.append(path+os.path.basename(i) + '-rotate-' + '.bmp')
+		g.transform(psMat.rotate(.0523599)).export(path + os.path.basename(i)+'-rotate-'+ str(g.unicode)+ '.bmp')
+		list_of_files.append(path+os.path.basename(i) + '-rotate-' + str(g.unicode)+ '.bmp')
 
-		g.transform(psMat.translate(1,1)).export(path + os.path.basename(i) + '-translate-' + '.bmp')
-		list_of_files.append(path+os.path.basename(i) + '-translate-' + '.bmp')
+		g.transform(psMat.translate(1,1)).export(path + os.path.basename(i) + '-translate-' +str(g.unicode)+'.bmp')
+		list_of_files.append(path+os.path.basename(i) + '-translate-' +str(g.unicode)+ '.bmp')
 
                 
                 for f in list_of_files:
